7613784215582e48edb08c1dd3479de18458046b
Igor Ergin <gora.ergin@gmail.com>
2026-09-24 19:44:59 +0500

    Add domain value classes for core domain identifiers
    - Add `RepoId`, `UserId`, `OwnerName`, `RepoName`, and `Sha` `@JvmInline` value classes to `ValueClasses.kt` in `:core:domain`.

---
+++ core/domain/src/main/java/com/ergin/ghclient/core/domain/model/ValueClasses.kt
@@ -0,0 +1,16 @@
+package com.ergin.ghclient.core.domain.model
+
+@JvmInline
+value class RepoId(val value: Long)
+
+@JvmInline
+value class UserId(val value: Long)
+
+@JvmInline
+value class OwnerName(val value: String)
+
+@JvmInline
+value class RepoName(val value: String)
+
+@JvmInline
+value class Sha(val value: String)
